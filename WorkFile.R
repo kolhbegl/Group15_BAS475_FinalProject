e32b9306add45e7f6b709923be81536f2baa44f9
Kolton Begley <99286975+kolhbegl@users.noreply.github.com>
2022-04-27 15:48:10 -0400

    Update WorkFile.R
    Added month variable and created the tsibble

--- WorkFile.R
+++ WorkFile.R
@@ -4,7 +4,13 @@ library(lubridate)
 
 Credits <- read.csv("credit.csv")
 
+Credits %>% 
+  mutate(Month = c(1:492)) %>% 
+  as_tsibble(index = Month) -> Empire_Credits
 
+
+Empire_Credits %>% 
+  autoplot(credit_in_millions)
 
 
 
