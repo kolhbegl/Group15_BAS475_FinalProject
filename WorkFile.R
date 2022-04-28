7b42fe69d1467189b44087653b4450d1dda372d2
Kolton Begley <99286975+kolhbegl@users.noreply.github.com>
2022-04-27 21:30:32 -0400

    Update WorkFile.R

--- WorkFile.R
+++ WorkFile.R
@@ -6,13 +6,17 @@ Credits <- read.csv("credit.csv")
 
 Credits %>% 
   mutate(Month = c(492:1)) %>% 
-  as_tsibble(index = Month) -> Empire_Credits
+  as_tsibble(index = Month) -> Empire_Credits1
 
 
-Empire_Credits %>% 
+Empire_Credits1 %>% 
   autoplot(credit_in_millions)
 
+Empire_Credits2 <- Empire_Credits1
 
+Empire_Credits3 <- Empire_Credits1
+
+Empire_Credits4 <- Empire_Credits1
 
 
 
