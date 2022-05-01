ae9e5251f3fb900b645bb0463e955eeb7e28427b
Shiv-Brahmbhatt <shivb2000@gmail.com>
2022-05-01 18:19:16 -0400

    Update WorkFile_Shiv.R
    Created Box-Cox and autoplo

--- WorkFile_Shiv.R
+++ WorkFile_Shiv.R
@@ -10,7 +10,23 @@ Credits %>%
 
 
 Empire_Credits %>% 
-  autoplot(credit_in_millions)
+  autoplot(credit_in_millions) +
+  labs(title = "Empire Credits Original")
+
+# Pull Lamdba
+Empire_Credits %>% 
+  select(credit_in_millions) %>% 
+  features(credit_in_millions, features = guerrero) %>%
+  pull(lambda_guerrero) -> lambda
+
+# Box-Cox Transformation
+Empire_Credits %>%
+  select(credit_in_millions) %>% 
+  autoplot(box_cox(credit_in_millions,lambda )) +
+  labs(y = "",
+       title = 
+         "Transformed Credit with Lambda",
+         round(lambda,2))
 
 
 
